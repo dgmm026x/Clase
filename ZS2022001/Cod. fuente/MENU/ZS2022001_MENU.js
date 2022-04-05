5a180a0497bd3fbd8f4344d5abe0068165c75d35
dgmm026x <dgm0026x@gmail.com>
2022-04-05 14:17:07 +0200

    z

--- ZS2022001/Cod. fuente/MENU/ZS2022001_MENU.js
+++ ZS2022001/Cod. fuente/MENU/ZS2022001_MENU.js
@@ -39,23 +39,71 @@ function BURGUERMENU()
 
 
 
-// function BURGUERMENU()
-// {
-//   var STATUS = document.getElementById("BurguerContent");
-//   // var WIDTH = display.width;
+function THEME()
+{
+  const OPAQUE_ID = ["NavBar","themeSwitch"];
 
 
-//   if(STATUS.style.display === "block")
-//   {
-//     STATUS.style.display = "none";
-//   }
+  for (let i = 0; i < OPAQUE_ID.length; i++)
+  {
+    var O_ID=document.getElementById(OPAQUE_ID[i]);
+    console.log(i);
+    
+    if(O_ID.style.backgroundColor == "rgb(0, 0, 0)")
+    {O_ID.style.backgroundColor = "rgb(255, 255, 255)"; O_ID.style.color="rgb(0, 0, 0)";}
+    
+    else
+    {O_ID.style.backgroundColor="rgb(0, 0, 0)"; O_ID.style.color="rgb(255, 255, 255)";}
 
 
-//   else
-//   {
-//     STATUS.style.display = "block";
-//   }
-// }
+    const OPAQUE_CLASS = ["P"];
+
+
+    for (let i = 0; i < OPAQUE_CLASS.length; i++)
+    {
+      var O_CLASS=document.getComputedStyle(OPAQUE_CLASS[i]);
+      console.log(i);
+      
+      if(O_CLASS.style.backgroundColor == "rgb(0, 0, 0)")
+      {O_CLASS.style.backgroundColor = "rgb(255, 255, 255)"; O_CLASS.style.color="rgb(0, 0, 0)";}
+      
+      else
+      {O_CLASS.style.backgroundColor="rgb(0, 0, 0)"; O_CLASS.style.color="rgb(255, 255, 255)";}
+    }
+  }
+
+
+  // const TRANSLUCID_ID = ["BurguerContent"];
+
+
+  // for (let i = 0; i < TRANSLUCID_ID.length; i++)
+  // {
+  //   var ITEM=document.getElementById(TRANSLUCID_ID[i]);
+  //   console.log("b");
+
+  //   if(ITEM.style.backgroundColor == "rgba(0, 0, 0, 0.75)")
+  //   {ITEM.style.backgroundColor = "rgba(255, 255, 255, 0.75)"; ITEM.style.color = "rgb(0, 0, 0)";}
+    
+  //   else
+  //   {ITEM.style.backgroundColor = "rgba(0, 0, 0 0.75)"; ITEM.style.color = "rgb(255, 255, 255)";}
+  // }
+
+
+  // const TRANSPARENT_ID = [];
+
+
+  // for (let i = 0; i < TRANSPARENT_ID.length; i++)
+  // {
+  //   var ITEM=document.getElementById(TRANSPARENT_ID[i]);
+  //   console.log("c");
+
+  //   if(ITEM.style.color == "rgb(255, 255, 255)")
+  //   {ITEM.style.color = "rgb(0, 0, 0)";}
+    
+  //   else
+  //   {ITEM.style.color = "rgb(255, 255, 255)";}
+  // }
+}
 
 
 
