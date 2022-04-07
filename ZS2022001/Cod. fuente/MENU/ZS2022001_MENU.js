a4348af04d941e0294aefae0c36c6e1841040c20
dgmm026x <dgm0026x@gmail.com>
2022-04-07 14:29:44 +0200

    z

--- ZS2022001/Cod. fuente/MENU/ZS2022001_MENU.js
+++ ZS2022001/Cod. fuente/MENU/ZS2022001_MENU.js
@@ -21,98 +21,47 @@
 
 function BURGUERMENU()
 {
-  var STATUS = document.getElementById("BurguerWindow");
-  // var WIDTH = display.width;
+  var BURGUER = document.getElementById("BurguerWindow");
+  // var SCROLL = display.getElementById("BODY");
 
 
-  if(STATUS.style.display === "block")
+  if(BURGUER.style.display === "block")
   {
-    STATUS.style.display = "none";
-    console.log("Hiding burguer menu...");
+    BURGUER.style.display = "none";
+    console.log("Burguer menu (0)");
+    // SCROLL.style.position  = "none";
   }
 
 
   else
   {
-    STATUS.style.display = "block";
-    console.log("Showing burguer menu...");
+    BURGUER.style.display = "block";
+    console.log("Burguer menu (1)");
+    // SCROLL.style.position  = "fixed";
   }
 }
 
 
 
-function THEME()
+/*function THEME()
 {
-  const OPAQUE_ID = ["NavBar","themeSwitch","End"];
+  var THEME=document.getElementsByClassName("OPAQUE");
 
 
-  for (let i = 0; i < OPAQUE_ID.length; i++)
+  if(THEME.style.backgroundColor == "rgb(0, 0, 0)")
   {
-    var O_ID=document.getElementById(OPAQUE_ID[i]);
-    
-    
-    if(O_ID.style.backgroundColor == "rgb(0, 0, 0)")
-    {O_ID.style.backgroundColor = "rgb(255, 255, 255)"; O_ID.style.color="rgb(0, 0, 0)";}
-    
-
-    else
-    {O_ID.style.backgroundColor="rgb(0, 0, 0)"; O_ID.style.color="rgb(255, 255, 255)";}
-    console.log("a");
+    THEME.style.backgroundColor = "rgb(255, 255, 255)";
+    THEME.style.color="rgb(0, 0, 0)";
   }
 
 
-  const TRANSLUCID_ID = ["BurguerWindow"];
-
-
-  for (let i = 0; i < TRANSLUCID_ID.length; i++)
+  else
   {
-    var TL_ID=document.getElementById(TRANSLUCID_ID[i]);
-    
-
-    if(TL_ID.style.backgroundColor == "rgba(0, 0, 0, 0.75)")
-    {TL_ID.style.backgroundColor = "rgba(255, 255, 255, 0.75)"; TL_ID.style.color = "rgb(0, 0, 0)";}
-    
-
-    else
-    {TL_ID.style.backgroundColor = "rgba(0, 0, 0 0.75)"; TL_ID.style.color = "rgb(255, 255, 255)";}
-    console.log("b");
+    THEME.style.backgroundColor="rgb(0, 0, 0)";
+    THEME.style.color="rgb(255, 255, 255)";
   }
-
-
-  const TRANSPARENT_ID = [];
-
-
-  for (let i = 0; i < TRANSPARENT_ID.length; i++)
-  {
-    var T_ID=document.getElementById(TRANSPARENT_ID[i]);
-    
-
-    if(T_ID.style.color == "rgb(255, 255, 255)")
-    {T_ID.style.color = "rgb(0, 0, 0)";}
-    
-
-    else
-    {T_ID.style.color = "rgb(255, 255, 255)";}
-    console.log("c");
-  }
-}
-
-
-//   const OPAQUE_CLASS = ["P"];
-
-
-//   for (let i = 0; i < OPAQUE_CLASS.length; i++)
-//   {
-//     var O_CLSS=document.styleSheets(OPAQUE_CLASS[i]);
-//     console.log(i);
-    
-//     if(O_CLSS.style.backgroundColor == "rgb(0, 0, 0)")
-//     {O_CLSS.style.backgroundColor = "rgb(255, 255, 255)"; O_CLSS.style.color="rgb(0, 0, 0)";}
-    
-//     else
-//     {O_CLSS.style.backgroundColor = "rgb(0, 0, 0)"; O_CLSS.style.color = "rgb(255, 255, 255)";}
-//   }
-// }
+  console.log("a");
+}*/
 
 
 
